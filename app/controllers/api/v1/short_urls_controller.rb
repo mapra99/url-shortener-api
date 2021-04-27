3ccaa879042684f8ecb584dc0717cb0678e288fb
Miguel Prada <mapra99@gmail.com>
2021-04-27 01:20:40 -0500

    naive short_url create

--- app/controllers/api/v1/short_urls_controller.rb
+++ app/controllers/api/v1/short_urls_controller.rb
@@ -1,10 +1,23 @@
 class Api::V1::ShortUrlsController < Api::V1::ApplicationController
   def create
+    @short_url = ShortUrl.new(short_url_params)
+
+    if @short_url.save
+      render json: @short_url, status: 200
+    else
+      render json: { errors: @short_url.errors.full_messages }, status: 400
+    end
   end
 
   def stats
   end
 
   def redirect
+  end
+
+  private
+
+  def short_url_params
+    params.require(:short_url).permit(:original_url, :transformed_path)
   end
 end
