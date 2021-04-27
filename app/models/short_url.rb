3bab69689a7995efd49d93406f18050534771a02
Miguel Prada <mapra99@gmail.com>
2021-04-27 18:21:04 -0500

    refactored short_url validations

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -3,8 +3,12 @@ class ShortUrl < ApplicationRecord
 
   validates :original_url,
             presence: true,
+            format: { with: URI::DEFAULT_PARSER.make_regexp,
+                      message: 'must have valid format. ex: https://example.com' }
+
+  validates :transformed_path,
             uniqueness: true,
-            format: { with: URI::DEFAULT_PARSER.make_regexp, message: "must have valid format. ex: https://example.com" }
+            format: { with: %r{\A(/\S+|)\Z}i, message: 'must have valid format. ex: /custom-path?with=someParams' }
 
   before_save :generate_path
 
