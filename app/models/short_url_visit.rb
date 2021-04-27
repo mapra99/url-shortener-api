f6fca6cd23d75bd0eb0da36d2c0f43479432c36a
Miguel Prada <mapra99@gmail.com>
2021-04-27 00:11:03 -0500

    short url visit

--- app/models/short_url_visit.rb
+++ app/models/short_url_visit.rb
@@ -1,3 +1,16 @@
 class ShortUrlVisit < ApplicationRecord
   belongs_to :short_url
+
+  before_save :set_visited_at
+
+  scope :most_recent, -> { order(visited_at: :desc) }
+  scope :latest, -> { most_recent.first }
+
+  private
+
+  def set_visited_at
+    return if visited_at.present?
+
+    self.visited_at = Time.now
+  end
 end
